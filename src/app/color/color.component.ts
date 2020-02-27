88b821c0856148332a665a44d32bb437c6ac07d2
Nidhal Jelassi <nidhal@N2SY.local>
2020-02-27 19:45:08 +0100

    first commit

---
+++ src/app/color/color.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  section : string = "DMWM";
+  hd : boolean = true;
+  color = "pink"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  swipeHidden() {
+    this.hd = !this.hd;
+  }
+
+}
